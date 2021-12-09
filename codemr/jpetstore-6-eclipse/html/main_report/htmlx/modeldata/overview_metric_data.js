ad635a60a90ab61027087bae998fb8f6cb51210e
Muhammad Hafiz Hasan <mhafizhasan@gmail.com>
2021-12-09 17:03:05 +0800

    wip callgraph-maxi

---
+++ codemr/jpetstore-6-eclipse/html/main_report/htmlx/modeldata/overview_metric_data.js
@@ -0,0 +1,1 @@
+var CodeMR_ProjectMetrics = {"Project Name":"jpetstore-6", "Number of Highly Problematic Classes":0, "Number of Entities":24, "Number of Problematic Classes":1, "Class Lines of Code":963, "Number of External Packages":10, "Number of Packages":4, "Number of External Entities":40}
